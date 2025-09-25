4cd76edcec8d6eac4504cc0aad2ac40df680405e
AmrAnter44 <137618860+AmrAnter44@users.noreply.github.com>
2025-09-25 07:49:24 +0300

    asdsadas

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,11 @@ import { Oswald } from "next/font/google";
 import "./globals.css";
 import Fotter from "./comp/Fotter";
 import Nav from "./comp/Nav";
+
 const oswald = Oswald({
   variable: "--font-oswald",
   subsets: ["latin"],
-  weight: [ "300", "400", "500", "600", "700"], // كل الأوزان المتاحة
+  weight: ["300", "400", "500", "600", "700"], // كل الأوزان المتاحة
 });
 
 export const metadata = {
@@ -16,11 +17,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
+        {/* Preload لصورة الخلفية الكبيرة */}
+        <link rel="preload" href="/bg.webp" as="image" />
+
+        {/* لو محتاج stylesheet خارجي مثال */}
+        {/*
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+        */}
+      </head>
       <body className={`${oswald.variable} antialiased`}>
-        <Nav></Nav>
+        <Nav />
         {children}
-        <Fotter></Fotter>
+        <Fotter />
       </body>
     </html>
   );
