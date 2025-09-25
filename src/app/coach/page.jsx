9d1469581468223c216164f1f876688f5d9e4844
AmrAnter44 <137618860+AmrAnter44@users.noreply.github.com>
2025-09-25 18:58:35 +0300

    شسي

--- src/app/coach/page.jsx
+++ src/app/coach/page.jsx
@@ -573,7 +573,7 @@ export default function FitnessAgencyLanding() {
 
       {/* Floating Action Button */}
       <div className="fixed bottom-8 right-8 z-50">
-        <a href='https://wa.me/201028518754' className="w-16 h-16 bg-white text-black rounded-full flex items-center justify-center font-bold text-xl hover:bg-white hover:scale-110 transform transition-all duration-300 shadow-lg hover:shadow-xl animate-bounce hover:animate-none cursor-pointer">
+        <a href='https://wa.me/201143564657' className="w-16 h-16 bg-white text-black rounded-full flex items-center justify-center font-bold text-xl hover:bg-white hover:scale-110 transform transition-all duration-300 shadow-lg hover:shadow-xl animate-bounce hover:animate-none cursor-pointer">
           <MessageCircle className="w-6 h-6" />
         </a>
       </div>
